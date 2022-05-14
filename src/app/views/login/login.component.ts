35e2f9909964c0ef0a200b21cc27025b37aad4bb
Yuri Nascimento <yuripuc@hotmail.com>
2022-05-14 19:33:46 -0300

    SQL-UC10

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
@@ -10,9 +11,18 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginservice : LoginService , private router:Router) { }
+  
+
+  constructor(private loginservice : LoginService , private router:Router, private formBuilder: FormBuilder ) { }
+  loginForm:FormGroup;
+  email:String;
+  Password:String
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ["",[Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]]
+    })
   }
 
   loginModel = new User()
